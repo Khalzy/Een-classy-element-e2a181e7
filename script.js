32899653cff683efafed29bc219fa166edd75356
Candace <2100978@talnet.nl>
2020-03-02 15:10:01 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// document.querySelector("h1").innerText = "red";
-var h1 = document.querySelector("h1");
-// manipulate
-h1.style.color = "green";
-h1.innerText = "Change it up";
+const div = document.querySelector("h1");
+
+div.classList.add("Test");
